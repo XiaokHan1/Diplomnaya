6524521d106e37534059f9e298fe9c8d8ffc0fe5
wsrvs@outlook.com <wsrvs@outlook.com>
2023-03-02 09:33:49 +0300

    Добавьте файлы проекта.

---
+++ CreatureMonster/Helpers/BD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatureMonster.Helpers
+{
+    public class BD
+    {
+        public static Models.user24Entities user = new Models.user24Entities();
+    }
+}
