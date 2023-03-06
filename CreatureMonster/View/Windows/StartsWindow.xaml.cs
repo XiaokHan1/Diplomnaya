0fabc60b2f6df6764d63f6761ba658c2d217a86a
wsrvs@outlook.com <wsrvs@outlook.com>
2023-03-06 14:31:11 +0300

    1.2

--- CreatureMonster/View/Windows/StartsWindow.xaml.cs
+++ CreatureMonster/View/Windows/StartsWindow.xaml.cs
@@ -22,6 +22,58 @@ namespace CreatureMonster.View.Windows
         public StartsWindow()
         {
             InitializeComponent();
+            Text1.DataContext = Helpers.BD.user.Authorization.ToList();
+            Text2.DataContext = Helpers.BD.user.Authorization.ToList();
+
+            Cmb1.SelectedValuePath= "Id";
+            Cmb1.DisplayMemberPath = "Name";
+            Cmb1.ItemsSource = Helpers.BD.user.Body.ToList();
+
+            Cmb4.SelectedValuePath = "Id";
+            Cmb4.DisplayMemberPath = "Name";
+            Cmb4.ItemsSource = Helpers.BD.user.Tail.ToList();
+
+            Cmb2.SelectedValuePath = "Id";
+            Cmb2.DisplayMemberPath = "Name";
+            Cmb2.ItemsSource = Helpers.BD.user.Head.ToList();
+
+            Cmb3.SelectedValuePath = "Id";
+            Cmb3.DisplayMemberPath = "Name";
+            Cmb3.ItemsSource = Helpers.BD.user.Legs.ToList();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AuthRegWindows.Authorization authorization = new AuthRegWindows.Authorization();
+            authorization.Show();
+            Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string mes = "";
+            if (string.IsNullOrWhiteSpace(NameTb.Text))
+                mes += t1.Foreground = Brushes.Red;
+
+            if(mes == null)
+            {
+                mes = "";
+                MessageBox.Show(mes);
+                return;
+            }
+            Models.Creature creature = new Models.Creature()
+            {
+                Body = Cmb1.SelectedItem as Models.Body,
+                Tail = Cmb4.SelectedItem as Models.Tail,
+                Head = Cmb2.SelectedItem as Models.Head,
+                Legs = Cmb3.SelectedItem as Models.Legs,
+                Name_creature = NameTb.Text,
+                Id_authorization = 4
+            };
+            Helpers.BD.user.Creature.Add(creature);
+            Helpers.BD.user.SaveChanges();
+            MessageBox.Show("Yes");
         }
     }
 }
